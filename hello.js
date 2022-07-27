943447ad70b22ae1ca8dd4eb373a85e2f510f388
Abid Qadir <abidumrani222@gmail.com>
2022-07-28 02:35:51 +0500

    practice

---
+++ hello.js
@@ -0,0 +1,28 @@
+
+function calculator (a,b,ab){
+
+switch(ab){
+    case "-":
+        console.log(a-b)
+    break;
+   
+    case "*":
+    
+        console.log(a*b)
+    
+     case "/":
+            console.log(a/b)
+    break;
+    case "+":
+            console.log(a+b)
+        break;no
+           
+    }
+}
+    calculator(12,30,"-")
+    
+
+
+
+ 
+
