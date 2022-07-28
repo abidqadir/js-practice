180cd86432c6c5ab240c0ce0d2eede2ce66f1382
Abid Qadir <abidumrani222@gmail.com>
2022-07-28 17:14:58 +0500

    for off and for in

--- hello.js
+++ hello.js
@@ -1,28 +1,46 @@
+// let b = 5
 
-function calculator (a,b,ab){
+// do {
+//     console.log(b)
+//     b++
+// }
+// while ( false )
 
-switch(ab){
-    case "-":
-        console.log(a-b)
-    break;
-   
-    case "*":
-    
-        console.log(a*b)
-    
-     case "/":
-            console.log(a/b)
-    break;
-    case "+":
-            console.log(a+b)
-        break;no
-           
-    }
+
+// let mobile = {
+//     name:"8plus",
+//     model:'2018',
+//     colors:['red','blue','white'],
+
+//  wifi: () => 'hello',
+
+
+// bluetoth(){
+//     //ss
+// },
+
+// }
+
+//  console.log(mobile.bluetoth(),mobile.colors,mobile.name,mobile.wifi())
+
+
+/////for in loop
+
+
+// obj = {
+//     name:'abid',
+//     cast:'umrani',
+//     mob:'0300',
+
+// }
+
+// for (key in obj){
+//     console.log(obj[key])
+// }
+
+// for off looop
+
+let game = ['pubg','igi','cs'];
+for( t of game ){
+    console.log(t)
 }
-    calculator(12,30,"-")
-    
-
-
-
- 
-
