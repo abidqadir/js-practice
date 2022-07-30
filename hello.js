69c7c222df0410704b2048b8a680530fd375e8c7
Abid Qadir <abidumrani222@gmail.com>
2022-07-30 16:47:58 +0500

    calculator

--- hello.js
+++ hello.js
@@ -40,7 +40,143 @@
 
 // for off looop
 
-let game = ['pubg','igi','cs'];
-for( t of game ){
-    console.log(t)
+// let game = ['pubg','igi','cs'];
+// for( t of game ){
+//     console.log(t)
+// }
+
+// let circkt = [{name:'abid',cast:"umrani"},{name:'hamyoon',cast:'khosa'},{name:"bilal",cast:'golo'}];
+
+// for(tt of circkt){
+//     console.log(tt.name,tt.cast)
+// }
+ 
+// let a = true
+// for(b=1;b<=10; b++)
+// {
+//     if ( a==true ){
+//         console.log('true')
+//         a = false
+//     }
+// else
+// {
+//     console.log('false')
+//     a = true
+// }
+// }
+
+// for (let a =5;a>=1;a-- )
+// {
+//     if(a % 2 !==0) 
+//     console.log(a);
+// }
+
+// function myFunction(a)
+// {
+//     if ( a >=  80 && a <=100 ){
+//         console.log('A grade')
+//     }
+//     else if ( a >=70 && a< 80 ){
+//         console.log("b grade")
+//     }
+    
+//     else if ( a >=60 && a < 70 ){
+//         console.log("c grade")
+//     }
+//     else if ( a >=50 && a <60 ){
+//         console.log("d grade")
+//     }
+//     else 
+//     {
+//         console.log("fail")
+//     }
+// }
+
+//  myFunction(66)
+
+
+// function myFunction(a,b)
+// {
+    
+//     if(a>b){
+//         console.log( a+  " is greater number" )
+//     }   
+//     else if (a==b)
+//     {
+//         console.log(" both r equal")
+//     }
+//     else
+//     {
+//         console.log(b+ "  is greator number")
+//     }
+// }
+
+
+
+// function tellgreater(a,b){
+//     if(a>b)console.log(a +" is greater")
+//     else if(a==b) console.log("both are eqaul")
+//     else console.log(b +" is greater")
+// }
+
+// tellgreater(100,100)
+// myFunction(1,4)
+
+
+
+
+// function number(a)
+// {
+// if (a > 0 )
+//  console.log(a+ "  postive")
+
+// else if (a == 0)
+// console.log( a +" zero")
+
+// else console.log(a +" negitive")
+// }
+//  number(-1)
+
+// function calculator (n1,n2,op)
+// {
+//     if (op =='+'){
+//         console.log(n1+n2)
+//     } 
+//     else if (op =='-'){
+//         console.log(n1-n2)
+//     } 
+
+//     if (op =='*'){
+//         console.log(n1*n2)
+//     } 
+//     if (op =='/'){
+//         console.log(n1/n2)
+//     } 
+// }
+
+
+// calculator(2,2,"*")
+
+
+
+function calculator (a,b,c)
+{
+    if(c=="-"){
+        console.log(a-b)
+    }
+    else if (c == "+")
+    {
+        console.log(a+b)
+
+    }
+    if(c=="/"){console.log(a/b)}
+
+    if(c=="*") console.log(a*b)
 }
+
+calculator(2,0,"/")
+
+
+
+
+
