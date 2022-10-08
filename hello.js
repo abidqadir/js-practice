7e789e153b2855c7ed89ae19e155b03b8c80a13d
Abid Qadir <Abidumrani222@gmail.com>
2022-10-09 00:03:45 +0500

    text

--- hello.js
+++ hello.js
@@ -351,20 +351,20 @@
    
 //function number()
 //{
-  return - number
+ // return - number
 //}
 //number(6)
 
-
- function find_average(array) {
-    if (!array || array == 0){return 0}
-      let sum = 0
-      let total = 0
-     /      for (sum= 0;sum<array.length;sum++)
-      {
-      total += array[sum]  
-      }   
-      total = total / array.length
-     return total;
-    }
-vxcvxcvxcvxcvxcvxcvxcvxcxcs
+//
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
